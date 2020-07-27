8f1b1df7e6693194c8c3b573fe47a0c390d621c2
Hieu Nguyen <hieu.nt@briswell-vn.com>
2020-07-27 16:55:49 +0700

    update package.json

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,9 +9,9 @@ from 'graphql';
 import _ from 'lodash';
 
 const books = [
-  { name: 'Name book 1', genre: 'Fantasy', id: '1'},
-  { name: 'Name book 2', genre: 'Fantasy', id: '2'},
-  { name: 'Name book 3', genre: 'Sci-Fi', id: '3'},
+  { name: 'Name book 1', genre: 'Fantasy', id: '1', authorId: '1'},
+  { name: 'Name book 2', genre: 'Fantasy', id: '2', authorId: '2'},
+  { name: 'Name book 3', genre: 'Sci-Fi', id: '3', authorId: '3'},
 ];
 
 const authors = [
@@ -25,7 +25,14 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 
